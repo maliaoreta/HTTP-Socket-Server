618bcfe47530ddce1e8c82a42fc852c02afd3cc2
maliaoreta <maliaoreta@gmail.com>
2016-04-05 11:18:32 -1000

    reformats and removes console logs

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ var fs = require('fs');
 
 var currDate = new Date();
 var timestamp = currDate.toUTCString();
-
-
 var successHeader = 'HTTP/1.1 200 OK\n' + timestamp + '\nServer: MaliaServer';
 var failHeader = 'HTTP/1.1  404 NOT FOUND\n' + timestamp + '\nServer: MaliaServer';
 
 var server = net.createServer(function (request) {
 
   request.on('data', function (data) {
-    console.log('Data event has been fired');
 
     var statusLineUri = '.' + data.toString().split('\n')[0].split(' ')[1];
 
@@ -23,20 +20,13 @@ var server = net.createServer(function (request) {
       else {
         request.write(successHeader + '\n\n' + data.toString());
       }
-      
+
       request.end();
     });
-
-  });
-
-  request.on('end', function () {
-
-    console.log('socket ended');
   });
 });
 
 server.listen({port : 8080}, function () {
 
   var address = server.address();
-  console.log('Opened server on %d', address.port);
 });
