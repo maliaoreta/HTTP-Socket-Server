0c202954d07b32cc971b5bd21afa3dcc3a19050b
maliaoreta <maliaoreta@gmail.com>
2016-04-05 12:31:02 -1000

    adds validations for request methods and if uri is /

--- server.js
+++ server.js
@@ -5,24 +5,50 @@ var currDate = new Date();
 var timestamp = currDate.toUTCString();
 var successHeader = 'HTTP/1.1 200 OK\n' + timestamp + '\nServer: MaliaServer';
 var failHeader = 'HTTP/1.1  404 NOT FOUND\n' + timestamp + '\nServer: MaliaServer';
+var header405 = 'HTTP/1.1 405 METHOD NOT ALLOWED\n' + timestamp + '\nServer: MaliaServer';
 
 var server = net.createServer(function (request) {
 
   request.on('data', function (data) {
 
-    var statusLineUri = '.' + data.toString().split('\n')[0].split(' ')[1];
+    var statusLine = data.toString().split('\n')[0];
+    var statusLineUri = '.' + statusLine.split(' ')[1];
+    var statusLineMethod = statusLine.split(' ')[0];
 
-    fs.readFile(statusLineUri, function (err, data) {
+    if (statusLineMethod !== 'GET' && statusLineMethod !== 'HEAD') {
 
-      if (err) {
-        request.write(failHeader);
+      fs.readFile('./405.html', function (err, data) {
+
+        if (err) {
+          throw new Error(err);
+        }
+        else {
+          request.write(header405 + '\n\n' + data.toString());
+        }
+
+        request.end();
+      });
+    }
+    else {
+
+      if (statusLineUri === './') {
+        statusLineUri = './index.html';
       }
-      else {
-        request.write(successHeader + '\n\n' + data.toString());
-      }
 
-      request.end();
-    });
+      fs.readFile(statusLineUri, function (err, data) {
+
+        if (err) {
+          
+          return redirectTo404(request);
+        }
+        else {
+          
+          request.write(successHeader + '\n\n' + data.toString());
+        }
+
+        request.end();
+      });
+    }
   });
 });
 
@@ -30,3 +56,18 @@ server.listen({port : 8080}, function () {
 
   var address = server.address();
 });
+
+function redirectTo404 (request) {
+
+  fs.readFile('./404.html', function (err, data) {
+
+    if (err) {
+      throw new Error(err);
+    }
+    else {
+      request.write(failHeader + '\n\n' + data.toString());
+    }
+
+    request.end();
+  });
+};
